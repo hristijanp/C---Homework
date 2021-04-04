53005c5c434706c8be5c45f2c1e5a28756917582
hristijanp <78928927+hristijanp@users.noreply.github.com>
2021-04-04 20:35:51 +0200

    homework3 added

---
+++ HomeworkC3/Homework3.1/Homework3.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Homework3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Make a console application called SumOfEven. Inside it create an array of 6 integers. Get numbers from the input, find and print the sum of the even numbers from the array:
+            Console.WriteLine("======sumOfEven======");
+
+            int[] integer = new int[6];
+            int sum = 0;
+
+            Console.WriteLine("Enter 6 numbers and I will return the sum of the even numbers");
+            for(int i = 0; i < integer.Length; i++)
+            {
+                Console.WriteLine("Enter number " + (i + 1));
+                bool number = int.TryParse(Console.ReadLine(), out integer[i]);
+                if(number)
+                {
+                    if (integer[i] % 2 == 0) sum += integer[i];
+                    else continue;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }
+            }
+            Console.WriteLine("The sum of the even numbers is: " + sum);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HomeworkC3/Homework3.2/Homework3.2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Homework3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Make a new console application called StudentGroup
+            //Make 2 arrays called studentsG1 and studentsG2 and fill them with 5 student names.
+            // Get a number from the console between 1 and 2 and print the students from that group in the console
+            Console.WriteLine("======StudentGroup======");
+
+            string[] studentsG1 = new string[]
+            {
+                "Goran",
+                "Zoran",
+                "Rade",
+                "Marija",
+                "Nikolina"
+            };
+
+            string[] studentsG2 = new string[]
+            {
+                "Blagica",
+                "Zorica",
+                "Vasko",
+                "Miki",
+                "Ivana"
+            };
+
+            Console.WriteLine("Enter 1 to see the students in G1, enter 2 to see the students in G2");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine("The students in G1 are: ");
+                        foreach (string item in studentsG1) 
+                    {
+                        Console.WriteLine(item);
+                    }
+                    break;
+                case "2":
+                    Console.WriteLine("The students in G2 are: ");
+                        foreach(string item2 in studentsG2)
+                    {
+                        Console.WriteLine(item2);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("You have to enter 1 or 2");
+                    break;
+            }
+
+            Console.ReadLine();
+   }    }   
+}
